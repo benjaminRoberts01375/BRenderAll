4b6a664d2c232382ffc3d928cc0d99e47fc4b2d2
Ben Roberts <benjamin.roberts01@mymail.champlain.edu>
2023-09-05 20:16:39 -0400

    Enforce single directory

--- RenderAll.sh
+++ RenderAll.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 # Get a directory to folder of Blend files
+USAGE="Usage: $0 [Path to directory]"
+
 if [ $# -ne 1 ]; then
-	echo "Usage: $0" [Path]
+	echo "Incorrect argument count"
+	echo $USAGE
+	exit 1
+elif [[ ! -d $1 ]]; then
+	echo "Provided path is not a directory"
+	echo $USAGE
 	exit 1
 fi
