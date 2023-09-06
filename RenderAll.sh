8fb982ce498c7a9e9f73f68930c78ce251f5031c
Ben Roberts <benjamin.roberts01@mymail.champlain.edu>
2023-09-05 20:30:04 -0400

    Declare number of found Blend files

--- RenderAll.sh
+++ RenderAll.sh
@@ -12,3 +12,13 @@ elif [[ ! -d $1 ]]; then
 	echo $USAGE
 	exit 1
 fi
+
+project_folder="$1" # A funner name
+
+# Declare how many were found
+file_count=$(find $project_folder -maxdepth 1 -type f -name "*.blend" | wc -l)
+if [[ $file_count == 1 ]]; then
+	echo "Found $file_count blend file"
+else
+	echo "Found $file_count blend files"
+fi
