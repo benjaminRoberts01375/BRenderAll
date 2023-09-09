59b07a151888f89d9ca6b13b116dbb889b41fb33
Ben Roberts <benjamin.roberts01@mymail.champlain.edu>
2023-09-09 15:43:11 -0400

    Correctly use user defined output directory.
    The -o flag is now actually doing something for the user. If the flag was not used (along with no path given for it), fall back to the project directory and create a folder called Renders

--- RenderAll.sh
+++ RenderAll.sh
@@ -50,20 +50,29 @@ while (($#)); do
 done
 
 # Check if an input was provided
-if [ $project_folder -z "" ]; then
+if [ -z $project_folder ]; then
 	echo "Usage $0 <Input directory> [flags]"
 	exit 1
 fi
 
 blender_executable="/home/benroberts/Desktop/blender-4.0.0-alpha+main.d45f47a809dd-linux.x86_64-release/blender" # Useful to specify a particular blender installation
-echo "$output_dir"
 
 # Declare how many were found
 file_count=$(find $project_folder -maxdepth 1 -type f -name "*.blend" | wc -l)
-if [[ $file_count == 1 ]]; then
+
+if [[ $file_count == 0 ]]; then
+	echo "No blend files were found in $project_folder."
+	exit 2
+elif [[ $file_count == 1 ]]; then
 	echo "Found $file_count blend file"
 else
 	echo "Found $file_count blend files"
+fi
+
+# Check if a specific output directory was passed
+if [[ -z $output_dir ]]; then
+	output_dir="$project_folder/Renders"
+	mkdir $output_dir -p
 fi
 
 finished_rendering=false
@@ -73,7 +82,7 @@ finished_rendering=false
 		project_file=$(basename -- "$project_file_path") # Name of project file without path leading to it (has extension still)
 		echo "Now rendering $project_file"
 		project_file_no_ext=${project_file%.blend} # Name of project file without extension
-		output_folder="$project_folder/Blender Renders/$project_file_no_ext"
+		output_folder="$output_dir/$project_file_no_ext"
 		find "$output_folder" -type f -size 0 -exec rm {} ';'
 		"$blender_executable" -b "$project_file_path" -o "$output_folder/$project_namme_no_ext" -a
 	done
