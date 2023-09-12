575034f9e0c77bfe38b5af588f0ffee2d014701c
Ben Roberts <benjamin.roberts01@mymail.champlain.edu>
2023-09-11 20:07:15 -0400

    Use frame range
    The frame range of a blend file is now considered when determining if a blend file needs to be rendered. Additionally, the user can now specify the frame range in the base arguments of .

--- RenderAll.sh
+++ RenderAll.sh
@@ -3,11 +3,22 @@
 output_dir=""
 blender_options=""
 project_folder=""
+check_frames=true
 
 while (($#)); do
 	case $1 in
+		-c)
+			check_frames=false
+			shift
+			;;
+
 		-a)
 			blender_options="-a $blender_options"
+			shift
+			;;
+		-e) # Specify end frame
+			blender_options="-e $2 $blender_options"
+			shift
 			shift
 			;;
 		-f)
@@ -51,6 +62,11 @@ while (($#)); do
 				done
 			fi
 			;;
+		-s) # Specify start frame
+			blender_options="-s $2 $blender_options"
+			shift
+			shift
+			;;
 		* )
 			if [[ -d $1 ]]; then
 				project_folder=$(realpath "$1")
@@ -90,14 +106,43 @@ if [[ -z $output_dir ]]; then
 fi
 
 finished_rendering=false
-#while [[ $finished_rendering != true ]]; do
+while [[ $finished_rendering != true ]]; do
 	# Render all available
 	for project_file_path in "$project_folder"/*.blend; do
 		project_file=$(basename -- "$project_file_path") # Name of project file without path leading to it (has extension still)
-		echo "Now rendering $project_file"
 		project_file_no_ext=${project_file%.blend} # Name of project file without extension
 		output_folder="$output_dir/$project_file_no_ext"
+		if [[ ! -d $output_folder ]]; then
+			mkdir "$output_folder"
+		fi
+		rendered_frames=$(find "$output_folder" -type f -name "*.png" | wc -l)
+		# Check to ensure all frames have been rendered
+		if [ $check_frames = true ] && [ $rendered_frames -gt 0 ] ; then
+			# Output the first and last frame to render
+			frame_output=$("$blender_executable" -b "$project_file_path" --python-expr "import bpy; print(bpy.context.scene.frame_end); print(bpy.context.scene.frame_start)" )
+			start_frame=$(echo "$frame_output" | awk 'NR==2')
+			end_frame=$(echo "$frame_output" | awk 'NR==1')
+			# Override if frame range has been specified
+			if [[ $blender_options == *-s* ]]; then
+				echo "Override start frames"
+				start_frame=$(echo "$frame_output" | grep -oP '(?<=-s\s)\d+') # Grep pearl expression to look for a "-s NUM"
+			fi
+			if [[ $blender_options == *-e* ]]; then
+				echo "Override end frames"
+				end_frame=$(echo "$frame_output" | grep -oP '(?<=-e\s)\d+') # Grep pearl expression to look for a "-e NUM"
+			fi
+			if (( rendered_frames == end_frame - start_frame + 1)); then
+				finished_rendering=true
+				echo "Done rendering $project_file_no_ext"
+				continue
+			else
+				finished_rendering=false
+			fi
+		fi
+
+		echo "Now rendering $project_file"
 		find "$output_folder" -type f -size 0 -exec rm {} ';' > /dev/null
 		"$blender_executable" -b "$project_file_path" -o "$output_folder/$project_name_no_ext" $blender_options
 	done
-#done
+	finished_rendering=true # Temp until "double checking" is implemented
+done
